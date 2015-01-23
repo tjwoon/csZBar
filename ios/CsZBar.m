dc04869ae1778bad57f4973cb78cc84f82330e89
PaoloMessina <ecompliance@Mac-mini-di-eCompliance.local>
2015-01-23 10:36:49 +0100

    iOS adding landscape sight
    Adding landscape sight for iOS terminal, bug fix on iPad

--- ios/CsZBar.m
+++ ios/CsZBar.m
@@ -1,12 +1,12 @@
 #import "CsZBar.h"
-
+#import "AlmaZBarReaderViewController.h"
 
 #pragma mark - State
 
 @interface CsZBar ()
 @property bool scanInProgress;
 @property NSString *scanCallbackId;
-@property ZBarReaderViewController *scanReader;
+@property AlmaZBarReaderViewController *scanReader;
 @end
 
 
@@ -40,10 +40,10 @@
     } else {
         self.scanInProgress = YES;
         self.scanCallbackId = [command callbackId];
-        self.scanReader = [ZBarReaderViewController new];
+        self.scanReader = [AlmaZBarReaderViewController new];
 
         self.scanReader.readerDelegate = self;
-        self.scanReader.supportedOrientationsMask = ZBarOrientationMaskAll;
+        self.scanReader.supportedOrientationsMask = ZBarOrientationMask(UIInterfaceOrientationPortrait);
 
         // Get user parameters
         NSDictionary *params = (NSDictionary*) [command argumentAtIndex:0];
@@ -70,12 +70,12 @@
             CGFloat screenWidth = screenRect.size.width;
             CGFloat screenHeight = screenRect.size.height;
             CGFloat dim = screenWidth < screenHeight ? screenWidth / 1.1 : screenHeight / 1.1;
-            UIView *polygonView = [[UIView alloc] initWithFrame: CGRectMake ( 0, 0, dim, dim)];
-            polygonView.center = self.scanReader.view.center;
-            polygonView.layer.borderColor = [UIColor greenColor].CGColor;
-            polygonView.layer.borderWidth = 3.0f;
+            UIView *polygonView = [[UIView alloc] initWithFrame: CGRectMake ( (screenWidth/2) - (dim/2), (screenHeight/2) - (dim/2), dim, dim)];
+            //polygonView.center = self.scanReader.view.center;
+            //polygonView.layer.borderColor = [UIColor greenColor].CGColor;
+            //polygonView.layer.borderWidth = 3.0f;
             
-            UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, dim / 2, dim, 1)];
+            UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(dim / 2, 0, 1, dim)];
             lineView.backgroundColor = [UIColor redColor];
             [polygonView addSubview:lineView];
             
