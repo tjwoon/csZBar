de263d50b5d1e7371d2a1166aabd6165c916ae6c
Daniel Cousens <github@dcousens.com>
2016-04-22 13:53:26 +1000

    Android: format && cleanup

--- android/ZBar.java
+++ android/ZBar.java
@@ -13,66 +13,55 @@ import android.content.Context;
 import org.cloudsky.cordovaPlugins.ZBarScannerActivity;
 
 public class ZBar extends CordovaPlugin {
+	// Configuration
+	private static int SCAN_CODE = 1;
 
-    // Configuration ---------------------------------------------------
+	// State
+	private boolean isInProgress = false;
+	private CallbackContext scanCallbackContext;
 
-    private static int SCAN_CODE = 1;
+	// Plugin API
+	@Override
+	public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (!action.equals("scan")) return false;
 
+		if (isInProgress) {
+			callbackContext.error("A scan is already in progress!");
+		} else {
+			isInProgress = true;
+			scanCallbackContext = callbackContext;
+			JSONObject params = args.optJSONObject(0);
 
-    // State -----------------------------------------------------------
+			Context appCtx = cordova.getActivity().getApplicationContext();
+			Intent scanIntent = new Intent(appCtx, ZBarScannerActivity.class);
+			scanIntent.putExtra(ZBarScannerActivity.EXTRA_PARAMS, params.toString());
+			cordova.startActivityForResult(this, scanIntent, SCAN_CODE);
+		}
 
-    private boolean isInProgress = false;
-    private CallbackContext scanCallbackContext;
+		return true;
+	}
 
+	// External results handler
+	@Override
+	public void onActivityResult (int requestCode, int resultCode, Intent result) {
+		if (requestCode == SCAN_CODE) {
+			switch(resultCode) {
+				case Activity.RESULT_OK:
+					String barcodeValue = result.getStringExtra(ZBarScannerActivity.EXTRA_QRVALUE);
+					scanCallbackContext.success(barcodeValue);
+					break;
+				case Activity.RESULT_CANCELED:
+					scanCallbackContext.error("cancelled");
+					break;
+				case ZBarScannerActivity.RESULT_ERROR:
+					scanCallbackContext.error("Scan failed due to an error");
+					break;
+				default:
+					scanCallbackContext.error("Unknown error");
+			}
 
-    // Plugin API ------------------------------------------------------
-
-    @Override
-    public boolean execute (String action, JSONArray args, CallbackContext callbackContext)
-    throws JSONException
-    {
-        if(action.equals("scan")) {
-            if(isInProgress) {
-                callbackContext.error("A scan is already in progress!");
-            } else {
-                isInProgress = true;
-                scanCallbackContext = callbackContext;
-                JSONObject params = args.optJSONObject(0);
-
-                Context appCtx = cordova.getActivity().getApplicationContext();
-                Intent scanIntent = new Intent(appCtx, ZBarScannerActivity.class);
-                scanIntent.putExtra(ZBarScannerActivity.EXTRA_PARAMS, params.toString());
-                cordova.startActivityForResult(this, scanIntent, SCAN_CODE);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    // External results handler ----------------------------------------
-
-    @Override
-    public void onActivityResult (int requestCode, int resultCode, Intent result)
-    {
-        if(requestCode == SCAN_CODE) {
-            switch(resultCode) {
-                case Activity.RESULT_OK:
-                    String barcodeValue = result.getStringExtra(ZBarScannerActivity.EXTRA_QRVALUE);
-                    scanCallbackContext.success(barcodeValue);
-                    break;
-                case Activity.RESULT_CANCELED:
-                    scanCallbackContext.error("cancelled");
-                    break;
-                case ZBarScannerActivity.RESULT_ERROR:
-                    scanCallbackContext.error("Scan failed due to an error");
-                    break;
-                default:
-                    scanCallbackContext.error("Unknown error");
-            }
-            isInProgress = false;
-            scanCallbackContext = null;
-        }
-    }
+			isInProgress = false;
+			scanCallbackContext = null;
+		}
+	}
 }
