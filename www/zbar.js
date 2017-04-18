fdf6f1aa1ab507f415092ef31a8c1dc15aad73d4
Joan <joan@nextixsystems.com>
2017-04-18 14:44:29 +0800

    set default value for formats to empty array

--- www/zbar.js
+++ www/zbar.js
@@ -14,6 +14,7 @@ ZBar.prototype = {
         if(params.text_instructions === undefined) params.text_instructions = "Please point your camera at the QR code.";
         if(params.camera != "front") params.camera = "back";
         if(params.flash != "on" && params.flash != "off") params.flash = "auto";
+        if(!params.formats) params.formats = []; 
 
         exec(success, failure, 'CsZBar', 'scan', [params]);
     },
