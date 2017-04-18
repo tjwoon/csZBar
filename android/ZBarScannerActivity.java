541001e7fb9fea4d7ac8af636115f465344d564d
Joan <joan@nextixsystems.com>
2017-04-18 14:36:38 +0800

    bug fix

--- android/ZBarScannerActivity.java
+++ android/ZBarScannerActivity.java
@@ -154,7 +154,7 @@ implements SurfaceHolder.Callback {
 
 
             // Set the config for barcode formats
-            if (formats.length()){
+            if (formats.length() > 0){
 
                 //disable all 
                 for(ZBarcodeFormat format : getFormats()) {
