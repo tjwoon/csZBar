48f1d8cd6375c351bf8bb59828c9f68f1732c146
Eduardo Vieira <carlos.vieira@pmm.am.gov.br>
2017-07-27 16:50:05 -0400

    Melhoria no zBbar

--- android/ZBarScannerActivity.java
+++ android/ZBarScannerActivity.java
@@ -175,7 +175,7 @@ implements SurfaceHolder.Callback {
                 public void onSizeChanged (int w, int h, int oldW, int oldH) {
                     surfW = w;
                     surfH = h;
-                    matchSurfaceToPreviewRatio();
+                    //matchSurfaceToPreviewRatio();
                 }
             };
             scannerSurface.setLayoutParams(new FrameLayout.LayoutParams(
@@ -297,7 +297,7 @@ implements SurfaceHolder.Callback {
 
         surfW = w;
         surfH = h;
-        matchSurfaceToPreviewRatio();
+        //matchSurfaceToPreviewRatio();
 
         tryStopPreview();
         holder = hld;
