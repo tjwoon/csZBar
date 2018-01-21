0c98c3b6f4ca13ce80d0811666445bea032e9ac2
vikassingla <vikas@clerisysolutions.com>
2018-01-21 11:02:56 +0530

    removed extra code

--- android/CustomView.java
+++ android/CustomView.java
@@ -10,9 +10,9 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+// import android.graphics.Color;
+// import android.graphics.Paint;
+// import android.graphics.Rect;
 
 import ion.com.dmo2.R;
 
@@ -22,7 +22,7 @@ import static android.R.attr.right;
 
 
 class CustomView extends View {
-      private static final int CURRENT_POINT_OPACITY = 0xA0;
+//       private static final int CURRENT_POINT_OPACITY = 0xA0;
 
     public CustomView(Context context)
         {
@@ -39,18 +39,18 @@ class CustomView extends View {
      @Override
      protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-        paint.setColor(Color.BLACK);
-        paint.setAlpha(CURRENT_POINT_OPACITY);
+//         Paint paint = new Paint();
+//         int width = canvas.getWidth();
+//         int height = canvas.getHeight();
+//         paint.setColor(Color.BLACK);
+//         paint.setAlpha(CURRENT_POINT_OPACITY);
         Drawable d = getResources().getDrawable(R.drawable.scan_qrcode);
         d.setBounds(0, 0 , getRight(), getHeight());
         d.draw(canvas);
 
-        paint.setColor(Color.rgb(210,39,42));
-        paint.setStrokeWidth(3);
-        paint.setStyle(Paint.Style.STROKE);
+//         paint.setColor(Color.rgb(210,39,42));
+//         paint.setStrokeWidth(3);
+//         paint.setStyle(Paint.Style.STROKE);
         canvas.save();
      }
 }
